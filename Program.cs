f9b62c8bd8a00d5af60503a03b59552ce965073d
Rafael V. Wierzba <rafaelwierzba@gmail.com>
2024-09-10 15:04:00 -0300

    Iniciando o projeto

---
+++ Program.cs
@@ -0,0 +1,14 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Olá pessoal");
+
+app.MapPost("/login", (minimal_api.DTOs.Dominio.LoginDTO, loginDTO) => {
+  if(loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
+    return Results.Ok("Login realizado com sucesso!");
+  else
+    return Results.Unauthorized();  
+});
+
+app.Run();
+
